9ecc3dfc7acb3130e75e424bf215832e43f95ebc
allenaxie <allenaxie1@gmail.com>
2022-06-27 13:56:35 -0700

    Create notes API endpoint. Successfully send 'GET' method to Notes documents from MongoDB.

--- models/Note.js
+++ models/Note.js
@@ -1,21 +1,20 @@
 const mongoose = require('mongoose');
 
-const NoteSchema = new mongoose.Schema({
+const noteSchema = new mongoose.Schema({
     title: {
         type: String,
         required: [true, 'Please enter a title'],
         unique: [true, 'Title name must be unique'],
         trim: true,
-        maxLength: [40, 'Title can not contain more than 40 characters']
+        maxlength: [40, 'Title can not contain more than 40 characters']
     },
     description: {
         type: String,
         required: [true, 'Please enter a decription of the note'],
-        maxLength: [200, 'Description can not contain more than 200 characters']
+        maxlength: [200, 'Description can not contain more than 200 characters']
     },
-
 }, {
-    timestamps: true,
-})
+    timestamps: true
+});
 
-module.exports = mongoose.models.Note || mongoose.models('Note', NoteSchema)
+module.exports = mongoose.models.Note || mongoose.model('Note', noteSchema);
---
+++ pages/api/notes/index.js
@@ -0,0 +1,38 @@
+import dbConnect from "../../../utilities/dbConnect";
+import Note from '../../../models/note';
+
+export default async function handler (req,res) {
+    const {method} = req;
+    
+    dbConnect();
+
+    switch(method) {
+        // if 'GET' method
+        case 'GET':
+            try {
+                // find all notes
+                const notes = await Note.find({});
+                console.log(`notes: ${notes}`);
+                res.status(200).json({success: true, data: notes});
+            } catch (error) {
+                console.log(error);
+                res.status(400).json({success: false})
+            }
+            break;
+        // if 'POST' method
+        case 'POST':
+            try {
+                // create note
+                const note = await Note.create(req.body);
+                res.status(201).json({success: true, data: note});
+            } catch (error) {
+                console.log(error);
+                res.status(400).json({success: false});
+            }
+            break;
+        // else
+        default: 
+            res.status(400).json({success: false});
+            break;
+    }
+}
